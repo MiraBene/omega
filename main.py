1b8d27af99d038332f4ad88c0e92d8232dbd5efa
MiraBene <benjamin.miraglio@oncoradiomics.com>
2018-11-02 14:16:15 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+
+__author__ = "Benjamin Miraglio"
+__copyright__ = "Copyright 2018, Benjamin Miraglio"
+__credits__ = "Benjamin Miraglio"
+
+__license__ = "GPL v3"
+__version__ = "0.1"
+__maintainer__ = "Benjamin Miraglio"
+__email__ = "benjamin@miraglio.fr"
+__status__ = "Prototype"
+
+
+if __name__ == '__main__':
+    os.getcwd()
